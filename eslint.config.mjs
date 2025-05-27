32e553eea5a160b10bf003097ff67323a403cd06
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-05-28 00:30:38 +0900

    enable @typescript-eslint/no-explicit-any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -41,7 +41,6 @@ export default [
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
       'prettier/prettier': [
         'error',
         {
